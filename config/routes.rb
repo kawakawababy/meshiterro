dbb325c0abfc53df8d48e8fa03bef7a2e6f6107e
kawakawababy <163483969+kawakawababy@users.noreply.github.com>
2024-04-24 18:01:34 +0000

    [Finish] 4-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'postimages/new'
-  get 'postimages/index'
-  get 'postimages/show'
-  devise_for :post_images
-  get 'homes/about' => 'homes#about', as: 'about'
-  devise_for :users
   root to: "homes#top"
+  devise_for :users
+
+  resources :post_images, only: [:new, :index, :show]
+
+  get 'homes/about' => 'homes#about', as: 'about'
 
 end
