165648354ff5b6531c7a40b53c30a39e4695c597
kawakawababy <163483969+kawakawababy@users.noreply.github.com>
2024-04-24 11:54:56 +0000

    [Pause] 4-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'postimages/new'
+  get 'postimages/index'
+  get 'postimages/show'
   devise_for :post_images
   get 'homes/about' => 'homes#about', as: 'about'
   devise_for :users
